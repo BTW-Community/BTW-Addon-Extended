83d4668ec329e228672d6fbf2ada83ec41f6f5c9
ammoore00 <abigailmmoore00@gmail.com>
2021-03-18 02:17:03 -0600

    Spawn eggs and translations for villagers

--- src/minecraft/net/minecraft/src/AddonVillagerHandler.java
+++ src/minecraft/net/minecraft/src/AddonVillagerHandler.java
@@ -5,10 +5,12 @@ public class AddonVillagerHandler extends FCAddOn {
 	
 	static {
 		EntityList.addMapping(AddonEntityVillagerFarmer.class, "addonVillagerFarmer", 600, 5651507, 12422002);
-		EntityList.addMapping(AddonEntityVillagerLibrarian.class, "addonVillagerLibrarian", 601, 5651507, 12422002);
-		EntityList.addMapping(AddonEntityVillagerPriest.class, "addonVillagerPriest", 602, 5651507, 12422002);
-		EntityList.addMapping(AddonEntityVillagerBlacksmith.class, "addonVillagerBlacksmith", 603, 5651507, 12422002);
-		EntityList.addMapping(AddonEntityVillagerButcher.class, "addonVillagerButcher", 604, 5651507, 12422002);
+		EntityList.addMapping(AddonEntityVillagerLibrarian.class, "addonVillagerLibrarian", 601, 14342874, 16179719);
+		EntityList.addMapping(AddonEntityVillagerPriest.class, "addonVillagerPriest", 602, 8470879, 12422002);
+		EntityList.addMapping(AddonEntityVillagerBlacksmith.class, "addonVillagerBlacksmith", 603, 4802889, 12422002);
+		EntityList.addMapping(AddonEntityVillagerButcher.class, "addonVillagerButcher", 604, 11447982, 12422002);
+		
+		EntityList.entityEggs.remove(120);
 	}
 
 	@Override
